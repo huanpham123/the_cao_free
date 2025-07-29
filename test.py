4b1cb89f47d4082c78cbb39ba6866b4416aaca7c
huanpham123 <158818634+huanpham123@users.noreply.github.com>
2025-07-29 10:32:33 +0700

    Update test.py

--- test.py
+++ test.py
@@ -4,10 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('b.html')  # HTML trắng nhưng tự động tải file
+    return render_template('index.html')
 
 @app.route('/download')
-def download():
+def download_file():
+    # Đường dẫn đến file .exe trong thư mục static
     return send_from_directory('static', 'helloworld.exe', as_attachment=True)
 
 if __name__ == '__main__':
